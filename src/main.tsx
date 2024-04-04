6132a50a54aa082d2f3560a385effdd1d11073ed
pangwenji <799953277@qq.com>
2024-04-04 20:43:04 +0800

    项目初始化

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import {
+  QueryClientProvider,
+  QueryClient
+} from '@tanstack/react-query';
+
+const Root = () => {
+const queryClient =new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient }>
+      <App />
+    </QueryClientProvider>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Root />
+)
